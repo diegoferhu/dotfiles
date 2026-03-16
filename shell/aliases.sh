4c2a395d89beda7cd3a414cd041f228e2550c8b4
diegoferhu <diegoferhu@gmail.com>
2026-03-16 11:54:57 -0500

    :sparkles: Update configurtaions
    Configurate toggleterm to use the basics configurations and update node
    path to use path of node version manage by volta

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -6,28 +6,24 @@ alias ...="cd ../.."
 alias ll="ls -l"
 alias la="ls -la"
 alias ~="cd ~"
-alias dotfiles='cd $DOTFILES_PATH'
 
 # Git
 alias gaa="git add -A"
-alias gc='$DOTLY_PATH/bin/dot git commit'
 alias gca="git add --all && git commit --amend --no-edit"
 alias gco="git checkout"
-alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
 alias gs="git status -sb"
 alias gf="git fetch --all -p"
 alias gps="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+alias gm="git merge ´branch to pull´ -m :twisted_rightwards_arrows: OD-branch: Sync with branch to pull"
 
 # Utils
 alias k='kill -9'
 alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
-alias up='dot package update_all'
 
 # occiCarro
 alias gofront='cd ~/Desktop/BOCC/vehiculos/front/bocc-occiauto-frontend-vehicles/'
@@ -38,5 +34,5 @@ alias gomicros='cd ~/Desktop/BOCC/vehiculos/front/micros/'
 
 alias initfront='cd ~/Desktop/BOCC/vehiculos/front/bocc-occiauto-frontend-vehicles/; npm start'
 # alias aws_token="sed -i '' 's/aws_session_token = .*/aws_session_token = $1/' ~/.aws/config"
-alias aws_token="sed -i 's/^aws_session_token = .*/aws_session_token = $1/' ~/.aws/config"
-alias assign_var="sed -i 's/^var=.*/var=\"$1\"/' ~/archivo.txt"
+# alias aws_token="sed -i 's/^aws_session_token = .*/aws_session_token = $1/' ~/.aws/config"
+# alias assign_var="sed -i 's/^var=.*/var=\"$1\"/' ~/archivo.txt"
